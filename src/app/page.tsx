950d36fe5d019db5b1a119548aad0ea1e5a40585
Luan Henrique <luanhenriquez10@hotmail.com>
2025-07-14 00:34:45 -0300

    progress preview added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { Box } from "@mui/material";
 import Header from "./components/Header";
 import QuickNavigation from "./components/QuickNavigation";
 import DailyTip from "./components/DailyTip";
+import ProgressPreview from "./components/ProgressPreview";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Header />
       <QuickNavigation />
       <DailyTip />
+      <ProgressPreview />
     </Box>
   );
 }
